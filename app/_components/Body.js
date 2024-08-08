ec1107da862580af7b5f2e1550bcf59a8520f16f
Marioo <marioyoussef936@gmail.com>
2024-08-08 17:09:56 +0300

    commit 4

--- app/_components/Body.js
+++ app/_components/Body.js
@@ -3,13 +3,16 @@ function Body() {
   return (
     <div className="h-[83vh] flex flex-col justify-center items-center gap-20">
       <h1 className="text-7xl text-white font-bold">UnCooked</h1>
-      <Image
-        src="/appstore.webp"
-        alt="Logo"
-        width={300}
-        height={100}
-        className=""
-      />
+      
+      <a href="https:/apple.com" target="_blank" rel="noopener noreferrer">
+          <Image
+            src="/appstore.webp"
+            alt="App Store"
+            width={300}
+            height={100}
+            className=""
+          />
+        </a>
     </div>
   );
 }
--- app/_components/Nabar.js
+++ app/_components/Nabar.js
@@ -6,28 +6,27 @@ function Navbar() {
     <div className="bg-white p-4">
       <div className="container mx-auto flex justify-between items-center font-bold">
         <div className="flex items-center ">
-          <div className="bg-purple-700 p-2 rounded-3xl">
+          <div className="bg-fuchsia-900 p-2 rounded-3xl">
             <Image
               src="/logo.png"
               alt="Logo"
-              width={50}
-              height={50}
+              width={45}
+              height={40}
               className="image-invert-purple"
             />
           </div>
-          <span className="text-purple-700 text-xl ml-3 sm:block hidden">Uncooked</span>
         </div>
-        <div className="flex sm:space-x-4 space-x-2 text-purple-700">
-          <Link href="/" className=" hover:text-purple-900">
+        <div className="flex sm:space-x-4 space-x-2 text-fuchsia-700">
+          <Link href="/" className=" hover:text-fuchsia-900">
             Home
           </Link>
-          <Link href="/About" className=" hover:text-purple-900">
+          <Link href="/About" className=" hover:text-fuchsia-900">
             About
           </Link>
-          <Link href="/Blog" className=" hover:text-purple-900">
+          <Link href="/Blog" className=" hover:text-fuchsia-900">
             Blog
           </Link>
-          <Link href="/Contact" className=" hover:text-purple-900">
+          <Link href="/Contact" className=" hover:text-fuchsia-900">
             Contact Us
           </Link>
         </div>
